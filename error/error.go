409285a592ec95c22f2da603b1847119672d98de
Eore <eore26@gmail.com>
2021-09-01 10:07:12 +0700

    feat: new error format

--- error/error.go
+++ error/error.go
@@ -3,51 +3,70 @@ package error
 import (
 	"encoding/json"
 	"fmt"
-	"runtime"
 )
 
-type Error struct {
-	ErrorType Type        `json:"error_type"`
-	Code      string      `json:"code"`
-	Err       string      `json:"error,omitempty"`
-	Input     interface{} `json:"input,omitempty"`
-	Output    interface{} `json:"output,omitempty"`
-	Location  string      `json:"location,omitempty"`
+type Err struct {
+	json.Marshaler
+	error
+	errorType Type
+	code      string
+	errors    []error
+	message   string
+	detail    map[string]interface{}
 }
 
 type Type string
 
 const (
-	Info Type = "INFO"
-	Warn Type = "WARNING"
-	Err  Type = "ERROR"
+	Info  Type = "INFO"
+	Warn  Type = "WARNING"
+	Error Type = "ERROR"
 )
 
-func NewError(errType Type, code string) Error {
-	return Error{
-		ErrorType: errType,
-		Code:      code,
+func NewError(errType Type, code string) Err {
+	return Err{
+		errorType: errType,
+		code:      code,
 	}
 }
 
-func (e Error) WithError(err error) Error {
-	e.Err = err.Error()
+func (e Err) WithError(err error) Err {
+	errCast, ok := err.(Err)
+	if ok {
+		e.errors = append(e.errors, errCast)
+	} else {
+		e.errors = append(e.errors, Err{
+			code:    "NATIVE_ERROR",
+			message: err.Error(),
+		})
+	}
 	return e
 }
 
-func (e Error) WithInput(input interface{}) Error {
-	e.Input = input
+func (e Err) WithMessage(message string) Err {
+	e.message = message
 	return e
 }
 
-func (e Error) WithOutput(output interface{}) Error {
-	e.Output = output
+func (e Err) WithDetail(name string, detaildata interface{}) Err {
+	e.detail[name] = detaildata
 	return e
 }
 
-func (e Error) ToJSON() []byte {
-	_, file, line, _ := runtime.Caller(1)
-	e.Location = fmt.Sprintf("%s:%d", file, line)
-	byteData, _ := json.Marshal(e)
-	return byteData
+func (e Err) ToMap() map[string]interface{} {
+	data := map[string]interface{}{
+		"code":    e.code,
+		"errors":  e.errors,
+		"message": e.message,
+		"detail":  e.detail,
+	}
+	return data
+}
+
+func (e Err) Error() string {
+	return fmt.Sprintf("[%s] %s", e.code, e.message)
+}
+
+func (e Err) MarshalJSON() ([]byte, error) {
+	return json.Marshal(e.ToMap())
 }
